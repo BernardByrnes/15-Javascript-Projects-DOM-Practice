ddf867727db148ac00d63fd82afb681a57af9e68
Bernard Byrnes <bernardtamboedu@gmail.com>
2025-10-03 18:10:17 +0300

    Fix sidebar transform for animation

---
+++ 15 javascript Project/05-sidebar/app.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  console.log(sidebar.classList);
+  // if (sidebar.classList.contains === "show-sidebar")
+  sidebar.classList.toggle("show-sidebar");
+});
