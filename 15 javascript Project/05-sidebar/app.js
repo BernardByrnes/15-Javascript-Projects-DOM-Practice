5cc364d2a058571f2b0f808b7c1d8cdf1c35d932
Bernard Byrnes <bernardtamboedu@gmail.com>
2025-10-04 16:25:56 +0300

    Add close-btn icon to sidebar

--- 15 javascript Project/05-sidebar/app.js
+++ 15 javascript Project/05-sidebar/app.js
@@ -3,7 +3,10 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  console.log(sidebar.classList);
-  // if (sidebar.classList.contains === "show-sidebar")
+  // console.log(sidebar.classList);
   sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
 });
